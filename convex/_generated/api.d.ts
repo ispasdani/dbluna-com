740123487f32c5ee5e3b0a18972ce19bbe2317b8
ispasdani <ispasdani14@gmail.com>
2025-12-18 18:21:46 +0100

    clerk + convex integrations established

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as guards from "../guards.js";
+import type * as http from "../http.js";
 import type * as sandbox from "../sandbox.js";
 import type * as tasks from "../tasks.js";
 import type * as users from "../users.js";
@@ -21,6 +22,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   guards: typeof guards;
+  http: typeof http;
   sandbox: typeof sandbox;
   tasks: typeof tasks;
   users: typeof users;
