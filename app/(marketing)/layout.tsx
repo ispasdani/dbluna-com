67ac91e01e79a01b4c499b334c5f8852269acba0
ispasdani <ispasdani14@gmail.com>
2025-12-18 17:34:56 +0100

    base clerk connection established

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,7 @@
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section className="h-screen w-full">{children}</section>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "./providers/ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const notoSans = Noto_Sans({ variable: "--font-sans" });
 
@@ -30,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ClerkProvider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
